efc72dd2642183b275a56bb2b3b0719b75be90b6
derya <deryabalin@gmail.com>
2023-07-03 17:50:27 +0300

    proje tamamlandı

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION, CLEAR_DISPLAY, ADD_TO_MEMORY, RECALL_MEMORY, CLEAR_MEMORY} from './../actions';
 
 export const initialState = {
   total: 100,
   operation: "*",
-  memory: 100
+  memory: 100,
+  display: ''
 }
 
 const calculateResult = (num1, num2, operation) => {
@@ -26,19 +27,36 @@ const reducer = (state, action) => {
         ...state,
         total: state.total + 1
       });
-
     case (APPLY_NUMBER):
       return ({
         ...state,
         total: calculateResult(state.total, action.payload, state.operation)
       });
-
     case (CHANGE_OPERATION):
       return ({
         ...state,
         operation: action.payload
       });
-
+    case (CLEAR_DISPLAY):
+      return ({
+        ...state,
+        total: 0
+      });
+    case (ADD_TO_MEMORY):
+      return ({
+        ...state,
+        memory: state.total
+      });
+    case (RECALL_MEMORY):
+      return ({
+        ...state,
+        total: calculateResult(state.total, state.memory, state.operation)
+      });
+    case (CLEAR_MEMORY):
+      return ({
+        ...state,
+        memory: 0
+      });
     default:
       return state;
   }
